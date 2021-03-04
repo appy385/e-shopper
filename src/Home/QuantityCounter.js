668c2909624fc4b9a79ab27ee7a658463f683fad
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-04 11:01:20 +0530

    feat(home): update `QuantityCounter.js` to display updated count of the item and add PropTypes

--- src/Home/QuantityCounter.js
+++ src/Home/QuantityCounter.js
@@ -1,16 +1,29 @@
-import { checkPropTypes } from 'prop-types';
+import PropTypes from 'prop-types';
+
 import React from 'react';
 
-import './QuantityCounter.css'
-function QuantityCounter({ count, onIncrement, onDecrement}) {
+import './QuantityCounter.css';
+
+function QuantityCounter({ count, onIncrement, onDecrement }) {
   return (
-      <div className="quantity">
-      <button  onClick={onIncrement}>+</button>
-       <p className="count">{count} in Basket </p>
-      <button onClick={onDecrement}>-</button> 
-      </div>
+    <div className="quantity">
+      <button onClick={onIncrement}>+</button>
+      <p className="count">
+        {count}
+        {' '}
+        in Basket
+        {' '}
+      </p>
+      <button onClick={onDecrement}>-</button>
+    </div>
   );
 }
 
+QuantityCounter.propTypes = {
+  count: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired,
+
+};
 
 export default QuantityCounter;
