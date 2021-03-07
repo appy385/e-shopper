83712ffddc657db7b0b84e3afb6037db144fa4bf
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-08 00:29:58 +0530

    feat(basket): add BasketCheckout.js file which calls signupForm

---
+++ src/Basket/BasketCheckout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './BasketCheckout.css';
+import SignupForm from '../Form/SignupForm';
+
+class BasketCheckout extends React.Component {
+  constructor() {
+    super();
+    this.setState = {};
+  }
+
+  render() {
+    return (
+      <div><SignupForm /></div>
+    );
+  }
+}
+
+export default BasketCheckout;
