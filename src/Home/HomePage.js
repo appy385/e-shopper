177a97c66d0905d94f4bbfe8678f256daafa1921
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-08 12:07:28 +0530

    refactor(Home): convert HomePage.js class to arrow function

--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -4,37 +4,26 @@ import Card from './Card';
 
 import './HomePage.css';
 
-class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    const { products, onIncrement, onDecrement } = this.props;
-
-    const allProducts = products.map((product) => (
-      <Card
-        key={product.id}
-        product={product}
-        onIncrement={() => onIncrement(product.id)}
-        onDecrement={() => onDecrement(product.id)}
-      />
-    ));
-    return (
-      <div className="home">
-        <div className="home-page">
-          <div className="home-category"><strong>Fruits and Vegetables</strong></div>
-          <div className="fruits-vegetables-row">
-            {allProducts}
-          </div>
+const HomePage = ({ products, onIncrement, onDecrement }) => {
+  const allProducts = products.map((product) => (
+    <Card
+      key={product.id}
+      product={product}
+      onIncrement={() => onIncrement(product.id)}
+      onDecrement={() => onDecrement(product.id)}
+    />
+  ));
+  return (
+    <div className="home">
+      <div className="home-page">
+        <div className="home-category"><strong>Fruits and Vegetables</strong></div>
+        <div className="fruits-vegetables-row">
+          {allProducts}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 HomePage.propTypes = {
   products: PropTypes.arrayOf(PropTypes.object).isRequired,
