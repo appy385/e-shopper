ed102f3ea2207cddb40b4acbef954a8176d6b197
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-10 11:24:31 +0530

    refactor(Basket): convert .css to .scss ext.

--- src/Basket/BasketPage.js
+++ src/Basket/BasketPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './BasketPage.css';
+import './BasketPage.scss';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Item from '../Item/Item';
