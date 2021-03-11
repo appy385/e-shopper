fecd811df0e05677c10f16d78596f6bd6f80f952
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-11 16:25:28 +0530

    fix(Header): update header.test.js file , add Browser Router to every test

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './Header';
 import { Theme, ThemeContext } from '../ThemeContext';
 
@@ -7,37 +8,43 @@ describe(Header.name, () => {
   const mockValue = 0;
   test('should match snapshot for theme light', () => {
     const { container } = render(
+
       <ThemeContext.Provider value={Theme.light}>
-        <Header value={mockValue} />
-      </ThemeContext.Provider>,
+        <BrowserRouter>
+          <Header value={mockValue} />
+        </BrowserRouter>
+      </ThemeContext.Provider>
+      ,
     );
     expect(container).toMatchSnapshot();
   });
   test('should match snapshot for theme dark', () => {
     const { container } = render(
       <ThemeContext.Provider value={Theme.dark}>
-        <Header value={mockValue} />
+        <BrowserRouter>
+          <Header value={mockValue} />
+        </BrowserRouter>
       </ThemeContext.Provider>,
     );
     expect(container).toMatchSnapshot();
   });
 
   test('should go to new route when E-shopper link is clicked', () => {
-    render(<Header value={mockValue} />);
+    render(<BrowserRouter><Header value={mockValue} /></BrowserRouter>);
     const eshopperLink = screen.getByText('E-shopper');
     fireEvent.click(eshopperLink);
     expect(document.location.href).toBe('http://localhost/');
   });
 
   test('should go to new route when All Orders link is clicked ', () => {
-    render(<Header value={mockValue} />);
+    render(<BrowserRouter><Header value={mockValue} /></BrowserRouter>);
     const allOrdersLink = screen.getByText('All Orders');
     fireEvent.click(allOrdersLink);
     expect(document.location.href).toBe('http://localhost/order');
   });
 
   test('should go to new route when Cart link is clicked ', () => {
-    render(<Header value={mockValue} />);
+    render(<BrowserRouter><Header value={mockValue} /></BrowserRouter>);
     const cartLink = screen.getByText(`Basket Items: ${mockValue}`);
     fireEvent.click(cartLink);
     expect(document.location.href).toBe('http://localhost/cart');
