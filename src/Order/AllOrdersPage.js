faf5b0dc378ea6d825146c1d449d0a87eb73f210
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-11 11:11:38 +0530

    feat(orders):  remove all static data , add prop orders , iterate over orders to display items. Also fix css and change classNames

--- src/Order/AllOrdersPage.js
+++ src/Order/AllOrdersPage.js
@@ -1,86 +1,78 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import './AllOrdersPage.scss';
 import Item from '../Item/Item';
 
-const AllOrdersPage = () => {
-  const orderProducts = [{
-    id: 1,
-    productName: 'Banana-Robusta',
-    price: 80.00,
-    count: 2,
-    category: 'Fruits & Vegatables',
-  },
-  {
-    id: 2,
-    productName: 'Apple',
-    price: 100.00,
-    count: 3,
-    category: 'Fruits & Vegatables',
-  },
-  {
-    id: 3,
-    productName: 'Mango',
-    price: 60.00,
-    count: 3,
-    category: 'Fruits & Vegatables',
-  }];
-  const total = orderProducts.reduce((acc, product) => (acc + product.price * product.count), 0);
-  return (
-    <div>
-      <p className="all-orders">All Orders</p>
-      <hr />
-      <p className="past-orders">Past Orders(10)</p>
-      <div className="order-table-container">
-        <table className="order-table">
-          <thead>
-          <tr className="order-table-header">
-            <th>Order</th>
-            <th>Item</th>
-            <th>Date</th>
-            <th>Amount</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr className="order-details">
-            <td>order id: 12</td>
-            <td>18 items</td>
-            <td>Sun 04 March 2018 10:01 pm</td>
-            <td>883.00</td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className="order-table-container">
-        <table className="order-item-table">
-        <thead>
-          <tr className="order-item">
-            <th>Item Description</th>
-            <th>Unit Price</th>
-            <th>Quantity</th>
-            <th>SubTotal</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr className="order-category">
-            <td>Fruits and vegetables</td>
-            <td> </td>
-            <td> </td>
-            <td> </td>
-          </tr>
-          {orderProducts.map((product) => (
-            <Item key={product.id} product={product} />
-          ))}
-          <tr className="item">
-            <td> </td>
-            <td> </td>
-            <td>Total:</td>
-            <td>{total}</td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-}
+const AllOrdersPage = ({ orders }) => (
+  <div>
+    <p className="all-orders">All Orders</p>
+    <hr />
+    <p className="past-orders">{`Past Orders(${orders.length})`}</p>
+    <div className="order-table-container">
+      {
+          orders.map((order) => {
+            const orderDate = new Date(order.date);
+            return (
+              <>
+                <table className="order-table">
+                  <thead>
+                    <tr className="order-table-header">
+                      <th>Order</th>
+                      <th>Item</th>
+                      <th>Date</th>
+                      <th>Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="order-details">
+                      <td>{`order id: ${order.id}`}</td>
+                      <td>{order.total}</td>
+                      <td>{(`${orderDate.getDate()}-${orderDate.getMonth()}-${orderDate.getFullYear()}`)}</td>
+                      <td>{order.cost}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div className="order-item-container">
+                  <table className="order-item-table">
+                    <thead>
+                      <tr className="order-item">
+                        <th>Item Description</th>
+                        <th>Unit Price</th>
+                        <th>Quantity</th>
+                        <th>SubTotal</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                         Object.entries(order.items).map((CategoryItems) => (
+                           <>
+                             <tr className="order-category">
+                               <td>{CategoryItems[0]}</td>
+                               <td> </td>
+                               <td> </td>
+                               <td> </td>
+                             </tr>
+                             { CategoryItems[1].map((item) => (
+                               <Item key={item.id} product={item} />
+                             ))}
+                           </>
+                         ))
+                      }
 
+                      <tr className="order-item-total">
+                        <td> </td>
+                        <td> </td>
+                        <td>Total:</td>
+                        <td>{order.cost}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </>
+            );
+          })
+       }
+    </div>
+  </div>
+);
 export default AllOrdersPage;
