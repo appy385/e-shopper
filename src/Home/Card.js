b639835805b505a7c7bab05dcba82c2d11f464b1
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-11 16:13:37 +0530

    feat(Home): add check for quantity count and display sold out if zero in Card.js  component

--- src/Home/Card.js
+++ src/Home/Card.js
@@ -8,16 +8,17 @@ const Card = (props) => {
   const {
     id, src, seller, name, quantity, price, count, category,
   } = product;
-
   return (
     <div className="product-card">
       <img src={src} alt="product" />
       <div className="product-seller">{seller}</div>
       <div className="product-name">{name}</div>
-      <div className="product-quantity">{}</div>
+      <div className="product-quantity">{`In stock: ${quantity}`}</div>
       <div className="product-card-box">
         <div className="product-price">{`MRP: ${price} /-`}</div>
-        <QuantityCounter count={count} onIncrement={onIncrement} onDecrement={onDecrement} />
+        { (quantity !== 0)
+          ? <QuantityCounter count={count} onIncrement={onIncrement} onDecrement={onDecrement} />
+          : <p className="product-sold-out">Sold out</p>}
       </div>
     </div>
   );
@@ -25,14 +26,14 @@ const Card = (props) => {
 
 Card.propTypes = {
   product: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    id: PropTypes.number,
     src: PropTypes.string.isRequired,
     seller: PropTypes.string,
     name: PropTypes.string.isRequired,
     quantity: PropTypes.number.isRequired,
     price: PropTypes.number.isRequired,
     count: PropTypes.number.isRequired,
-    category: PropTypes.string.isRequired,
+    category: PropTypes.string,
   }).isRequired,
   onIncrement: PropTypes.func.isRequired,
   onDecrement: PropTypes.func.isRequired,
