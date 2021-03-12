ff7559a5e1e4b6618a2a9b566ed4245a0cb0b072
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-12 15:38:02 +0530

    refactor(Basket): get mockBasket inside  BasketCheckoutPage.test.js from mockdata folder

--- src/Basket/BasketCheckoutPage.test.js
+++ src/Basket/BasketCheckoutPage.test.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import BaksetCheckout from './BasketCheckoutPage';
+import { mockAppBasket } from '../mockdata/app';
 
 describe(BaksetCheckout.name, () => {
   let mockBasket;
   let mockAddOrder;
   beforeEach(() => {
-    mockBasket = {
-      'Fruits & Vegatables': [{
-        id: 1,
-        src: 'https://images.pexels.com/photos/61127/pexels-photo-61127.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-        seller: 'Fresho',
-        name: 'Banana-Robusta',
-        price: 40,
-        count: 2,
-        category: 'Fruits & Vegatables',
-      }],
-    };
+    mockBasket = mockAppBasket;
     mockAddOrder = jest.fn();
   });
   test('shoudl match snapshot', () => {
