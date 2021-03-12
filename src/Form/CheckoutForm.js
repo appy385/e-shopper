8cb8e8bae8a1597e0137aaa07c84b38c222653e1
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-12 14:54:02 +0530

    feat(Form): use history to change route in CheckoutForm.js

--- src/Form/CheckoutForm.js
+++ src/Form/CheckoutForm.js
@@ -4,48 +4,52 @@ import {
   Formik, Field, Form, ErrorMessage,
 } from 'formik';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import './CheckoutForm.scss';
-import axios from 'axios';
+import { createOrder } from '../Utils/Api';
 
-const CheckoutForm = ({ basket, addOrder }) => (
-  <Formik
-    initialValues={{ firstName: '', lastName: '', email: '' }}
-    validationSchema={Yup.object({
-      firstName: Yup.string()
-        .max(15, 'Must be 15 characters or less')
-        .required('Required'),
-      lastName: Yup.string()
-        .max(20, 'Must be 20 characters or less')
-        .required('Required'),
-      email: Yup.string().email('Invalid email address').required('Required'),
-    })}
-    onSubmit={async (values, { setSubmitting }) => {
-      const response = await axios.post('/orders', basket);
-      addOrder(response.data.data);
-      setSubmitting(true);
-    }}
-  >
-    <Form className="checkout-form">
-      <div>
-        <label htmlFor="firstName">First Name:</label>
-        <Field name="firstName" id="firstName" type="text" />
-        <ErrorMessage name="firstName" />
-      </div>
-      <div>
-        <label htmlFor="lastName">Last Name:</label>
-        <Field name="lastName" id="lastName" type="text" />
-        <ErrorMessage name="lastName" />
-      </div>
-      <div>
-        <label htmlFor="email">Email Address:</label>
-        <Field name="email" id="email" type="email" />
-        <ErrorMessage name="email" />
-      </div>
+const CheckoutForm = ({ basket, addOrder }) => {
+  const history = useHistory();
+  return (
+    <Formik
+      initialValues={{ firstName: '', lastName: '', email: '' }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(15, 'Must be 15 characters or less')
+          .required('Required'),
+        lastName: Yup.string()
+          .max(20, 'Must be 20 characters or less')
+          .required('Required'),
+        email: Yup.string().email('Invalid email address').required('Required'),
+      })}
+      onSubmit={async (values, { setSubmitting }) => {
+        setSubmitting(true);
+        const response = await createOrder(basket);
+        addOrder(response);
+        history.push('/');
+      }}
+    >
+      <Form className="checkout-form">
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <Field name="firstName" id="firstName" type="text" />
+          <ErrorMessage name="firstName" />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <Field name="lastName" id="lastName" type="text" />
+          <ErrorMessage name="lastName" />
+        </div>
+        <div>
+          <label htmlFor="email">Email Address:</label>
+          <Field name="email" id="email" type="email" />
+          <ErrorMessage name="email" />
+        </div>
 
-      <button type="submit" id="submit">Submit</button>
-    </Form>
-  </Formik>
-);
+        <button type="submit" id="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
+};
 
 export default CheckoutForm;
