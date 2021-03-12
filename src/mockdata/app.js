4464e63c3d20adbf0bdc6188889185c4cc69bd4b
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-12 14:55:08 +0530

    feat(mockdata): create app.js and add  mock data for products

---
+++ src/mockdata/app.js
@@ -0,0 +1,25 @@
+export const mockAppProducts = {
+  'Fruits & Vegatables': [{
+    id: 1,
+    src: 'https://images.pexels.com/photos/61127/pexels-photo-61127.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    seller: 'Fresho',
+    name: 'Banana-Robusta',
+    quantity: 10,
+    price: 40,
+    count: 0,
+    category: 'Fruits & Vegatables',
+  },
+  {
+    id: 2,
+    src: 'https://images.pexels.com/photos/61127/pexels-photo-61127.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    seller: 'Fresho',
+    name: 'Banana-Organic',
+    quantity: 10,
+    price: 40,
+    count: 0,
+    category: 'Fruits & Vegatables',
+  },
+  ],
+};
+
+export const mockNew = {};
